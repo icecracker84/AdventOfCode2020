8c5cb777c613733ddba95040c7e8ae5819bf1107
Gelencsér Dávid <David.Gelencser@loxon.eu>
2020-12-16 08:49:17 +0100

    s

--- src/calendar/DayFifteen.java
+++ src/calendar/DayFifteen.java
@@ -3,17 +3,19 @@ package calendar;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.HashSet;
 
 
 public class DayFifteen {
 
 
-    private static final int MAXX = 3000;
-    private static final int startingTurn = 7;
+    private static final int MAXX = 30000001; // 30000000
+    private static int startingTurn = 0;
     private static Integer[] play = new Integer[MAXX];
     private static int ind = 1;
     private static HashSet<Integer> set = new HashSet<Integer>();
+    private static HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
 
 
     private static void readFile() {
@@ -42,8 +44,10 @@ public class DayFifteen {
             Integer val = Integer.valueOf(s);
             play[ind] = val;
             set.add(val);
+            map.put(val, ind);
             ind++;
         }
+        startingTurn = ind;
     }
 
 
@@ -52,40 +56,42 @@ public class DayFifteen {
 
         readFile();
 
-        while ( ind < 2021 ) {
+        while ( ind < MAXX ) {
             play[ind] = applyRule(ind);
-
             ind++;
         }
 
 
-        System.out.println("-------");
+        System.out.println(MAXX - 1 + " " + play[MAXX - 1] + "-------");
+        System.out.println(MAXX + " " + play[MAXX] + "-------");
 
-        int i = 1;
-        while ( i < 2021 ) {
-            System.out.println(i + ": " + play[i]);
-            i++;
-        }
+        // int i = 1;
+        // while ( i < MAXX ) {
+        // System.out.println(i + ": " + play[i]);
+        // i++;
+        // }
 
 
     }
 
+    static int percent = 0;
 
     private static int applyRule( final int i ) {
 
-        int j = i - 1;
-
+        if ( Math.floorMod(i, MAXX / 100) == 0 ) {
+            percent++;
+            System.out.println(percent + "%");
+        }
         if ( !(startingTurn == i) && set.contains(play[i - 1]) ) {
-            boolean found = false;
-            while ( !found ) {
-                j--;
-                if ( play[j].equals(play[i - 1]) ) {
-                    found = true;
-                }
-            }
-            return i - 1 - j;
+            Integer retVal = map.get(play[i - 1]);
+            map.replace(play[i - 1], i - 1);
+            return i - 1 - retVal;
         } else {
             set.add(play[i - 1]);
+            map.put(play[i - 1], i - 1);
+            if ( play[i - 1] <= startingTurn ) {
+                map.replace(play[i - 1], i - 1);
+            }
             return 0;
         }
     }
